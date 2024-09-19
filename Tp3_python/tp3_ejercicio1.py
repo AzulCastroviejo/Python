8af6955e932a35203f4e59b027cd26ac96479b9c
AzulCastroviejo <azulcastroviejoh3@gmail.com>
2024-09-19 20:18:37 -0300

    tp3

---
+++ Tp3_python/tp3_ejercicio1.py
@@ -0,0 +1,4 @@
+#1.Crear una Matriz de Números
+#Crea una función que reciba dos parámetros: el número de filas y columnas. La función 
+#debe generar una matriz de ese tamaño, donde los valores son números enteros 
+#consecutivos empezando desde 1.
