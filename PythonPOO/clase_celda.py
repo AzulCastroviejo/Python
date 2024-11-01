e5dc5498b217aa8e3041a536e21c2053d124d14c
AzulCastroviejo <azulcastroviejoh3@gmail.com>
2024-11-01 10:25:18 -0300

    Elimina carpeta

--- PythonPOO/clase_celda.py
+++
@@ -1,7 +0,0 @@
-
-
-class Celda:
-    def __init__(self,fila , columna , valor):
-        self.fila = fila
-        self.columna = columna
-        self.valor = valor
--- PythonPOO/clase_nota.py
+++
@@ -1,8 +0,0 @@
-#Clase: Nota 
-#Atributos: catedra (cadena), notaExamen(decimal) 
-
-class Nota:
-    
-    def __init__(self, catedra, nota_examen) :
-         self.catedra = catedra
-         self.nota_examen = nota_examen
